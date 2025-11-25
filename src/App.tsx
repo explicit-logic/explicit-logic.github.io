5c5d7424cea2689484179aadfb1db88005d30f4d
Bohdan Andrianov <bogd.an.drianov@gmail.com>
2025-11-25 22:53:01 +0100

    feat: add new Contact window component and integrate it into the main application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "~/components/Navbar";
 import { Welcome } from "~/components/Welcome";
 import { Dock } from "~/components/Dock";
+import { ContactWindow } from "~/windows/Contact";
 import { ResumeWindow } from "~/windows/Resume";
 import { TerminalWindow } from "~/windows/Terminal";
 import { SafariWindow } from "~/windows/Safari";
@@ -19,6 +20,7 @@ const App = () => {
       <Welcome />
       <Dock />
 
+      <ContactWindow />
       <FinderWindow />
       <ResumeWindow />
       <SafariWindow />
---
+++ src/windows/Contact.tsx
@@ -0,0 +1,43 @@
+import { WindowWrapper } from "~/hoc/WindowWrapper";
+import { socials } from "~/constants";
+import { WindowControls } from "~/components/WindowControls";
+
+export const Contact = () => {
+  return (
+    <>
+      <div id="window-header">
+        <WindowControls target="contact" />
+        <h2>Contact Me</h2>
+      </div>
+
+      <div className="p-5 space-y-5">
+        <img
+          src="images/adrian.jpg"
+          alt="Bohdan"
+          className="w-20 rounded-full"
+        />
+        <h3>Let's Connect</h3>
+        <p>Got an idea? Let's make it real!</p>
+        <p>Send me a message</p>
+        <p>explicit.logic@outlook.com</p>
+        <ul>
+          {socials.map((social) => (
+            <li key={social.id} style={{ backgroundColor: social.bg }}>
+              <a
+                href={social.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                title={social.text}
+              >
+                <img src={social.icon} alt={social.text} className="size-5" />
+                <p>{social.text}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export const ContactWindow = WindowWrapper(Contact, "contact");
